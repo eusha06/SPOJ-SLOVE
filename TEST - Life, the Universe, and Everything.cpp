2604f04f522c667066f3bc9297377f70074bb6fe
Eusha Hasan <113366884+eusha06@users.noreply.github.com>
2023-12-24 09:07:33 +0600

    TEST - Life, the Universe, and Everything.cpp

---
+++ TEST - Life, the Universe, and Everything.cpp
@@ -0,0 +1,17 @@
+/**
+    *       Author :  MD EUSHA HASAN, 
+    *       Created:  23-July-2023  20:00:13,
+   **/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+     int n;
+     cin>>n;
+     while (n!=42)
+     {
+        cout<<n<<endl;
+        cin>>n;
+     }
+    return 0;
+} 
